46022fc7fa2071794817ca093d81325d7426110a
OkabeRintarou <2573269786@qq.com>
2018-08-09 08:41:15 +0800

    Project0:Implementing a Multi-Client Echo Server

--- p0/src/github.com/cmu440/p0/server_impl.go
+++ p0/src/github.com/cmu440/p0/server_impl.go
@@ -2,28 +2,173 @@
 
 package p0
 
+import (
+	"net"
+	"strconv"
+	"bufio"
+)
+
+const (
+	msgConnect   = "connect"
+	msgClose     = "close"
+	msgBroadcast = "broadcast"
+)
+
+const writeBufferCapacity = 100
+const commandChanCapacity = 1024
+
 type multiEchoServer struct {
-	// TODO: implement this!
+	listener net.Listener
+
+	quitC       chan struct{}
+	acceptQuitC chan struct{}
+	commandC    chan command
+	connections map[*clientConn]bool
 }
 
 // New creates and returns (but does not start) a new MultiEchoServer.
 func New() MultiEchoServer {
-	// TODO: implement this!
-	return nil
+	return &multiEchoServer{
+		quitC:       make(chan struct{}, 1),
+		acceptQuitC: make(chan struct{}, 1),
+		commandC:    make(chan command, commandChanCapacity),
+		connections: make(map[*clientConn]bool),
+	}
 }
 
 func (mes *multiEchoServer) Start(port int) error {
-	// TODO: implement this!
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return err
+	}
+	mes.listener = listener
+	go mes.start()
+	go mes.handleMessage()
 	return nil
 }
 
 func (mes *multiEchoServer) Close() {
-	// TODO: implement this!
+	mes.quitC <- struct{}{}
 }
 
 func (mes *multiEchoServer) Count() int {
-	// TODO: implement this!
-	return -1
+	return len(mes.connections)
 }
 
-// TODO: add additional methods/functions below!
+func (mes *multiEchoServer) handleMessage() {
+	for {
+		select {
+		case cmd := <-mes.commandC:
+			switch cmd.cmd {
+			case msgConnect:
+				conn := cmd.data.(*clientConn)
+				mes.connections[conn] = true
+			case msgClose: // close connection between client
+				conn := cmd.data.(*clientConn)
+				conn.close()
+				delete(mes.connections, conn)
+			case msgBroadcast:
+				msg := cmd.data.([]byte)
+				for conn := range mes.connections {
+					conn.write(msg)
+				}
+			default:
+				panic("invalid command " + cmd.cmd)
+			}
+		case <-mes.quitC:
+			for conn := range mes.connections {
+				conn.close()
+			}
+			mes.listener.Close()
+			mes.acceptQuitC <- struct{}{}
+			return
+		}
+	}
+
+}
+
+func (mes *multiEchoServer) start() {
+
+	for {
+		select {
+		case <-mes.acceptQuitC:
+			return
+		default:
+			conn, err := mes.listener.Accept()
+			if err != nil {
+				continue
+			}
+			go newConn(conn, mes.commandC).start()
+		}
+	}
+}
+
+type clientConn struct {
+	conn     net.Conn
+	reader   *bufio.Reader
+	commandC chan<- command
+	writeC   chan []byte
+	quitC    chan struct{}
+}
+
+func newConn(conn net.Conn, commandC chan<- command) *clientConn {
+	return &clientConn{
+		conn:     conn,
+		reader:   bufio.NewReader(conn),
+		commandC: commandC,
+		writeC:   make(chan []byte, writeBufferCapacity),
+		quitC:    make(chan struct{}, 1),
+	}
+}
+
+func (conn *clientConn) close() {
+	conn.quitC <- struct{}{}
+	conn.conn.Close()
+}
+
+func (conn *clientConn) write(msg []byte) {
+	select {
+	case conn.writeC <- msg:
+	default:
+	}
+}
+
+func (conn *clientConn) start() {
+
+	defer func() {
+		select {
+		case conn.commandC <- command{cmd: msgClose, data: conn}:
+		default:
+		}
+	}()
+
+	select {
+	case conn.commandC <- command{cmd: msgConnect, data: conn}:
+	case <-conn.quitC:
+		return
+	}
+
+	go func() {
+		for {
+			select {
+			case msg := <-conn.writeC:
+				conn.conn.Write([]byte(msg))
+			case <-conn.quitC:
+				return
+			}
+		}
+	}()
+
+	for {
+		text, err := conn.reader.ReadBytes('\n')
+		if err != nil {
+			return
+		}
+		conn.commandC <- command{cmd: msgBroadcast, data: text}
+	}
+}
+
+type command struct {
+	cmd  string
+	data interface{}
+}
