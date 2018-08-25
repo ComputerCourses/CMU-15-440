b5abae68c7f9740d64381babef75714b4e3e0302
OkabeRintarou <2573269786@qq.com>
2018-08-25 19:00:58 +0800

    Project 1: Pass test 1 and test 2

--- p1/src/github.com/cmu440/lsp/client_impl.go
+++ p1/src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,72 @@
 
 package lsp
 
-import "errors"
+import (
+	"errors"
+	"time"
+	"log"
+	"encoding/json"
+	"container/list"
+	"sync/atomic"
+	"net"
+	"github.com/cmu440/lspnet"
+)
+
+const debugOn = false
+
+func debug(format string, v ...interface{}) {
+	if debugOn {
+		log.Printf(format, v...)
+	}
+}
+
+const (
+	stateConnecting int32 = iota
+	stateConnected
+	stateClosing
+	stateClosed
+)
+
+const (
+	defaultClientSendChanCapacity    = 1024
+	defaultClientReceiveChanCapacity = 1024
+)
+
+type inFlyMessage struct {
+	expireTime time.Time
+	epochCount int
+	msg        *Message
+}
+
+func newInFlyMessage(expireTime time.Time, epochCount int, msg *Message) *inFlyMessage {
+	return &inFlyMessage{expireTime, epochCount, msg}
+}
 
 type client struct {
-	// TODO: implement this!
+	connID int
+
+	seqNum     int
+	windowSize int
+
+	pendingBuf *list.List
+	sendBuf    map[int]*inFlyMessage // message buffer that has been sent but not receive ack
+	lastAckSeq int
+
+	receiveBuf      *list.List // store unordered message received from peer
+	receivedDataSeq int        // largest data seq has received and sent to application
+
+	params *Params
+
+	receiveMsgC  chan *Message
+	sendMsgC     chan *Message
+	receivedMsgC chan *Message // message already received from peer, but not has been read by application
+
+	closeC  chan struct{} // Close function notify background goroutine to quit
+	notifyC chan struct{} // background goroutine notify Close function the goroutine has quit
+
+	timer     *time.Timer
+	conn      *lspnet.UDPConn
+	connState int32
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -19,23 +81,445 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+
+	udpAddr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := lspnet.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &client{
+		conn:       c,
+		seqNum:     0,
+		params:     params,
+		windowSize: params.WindowSize,
+
+		pendingBuf: list.New(),
+		sendBuf:    make(map[int]*inFlyMessage),
+
+		receiveBuf: list.New(),
+
+		connState: stateConnecting,
+
+		receiveMsgC:  make(chan *Message, defaultClientReceiveChanCapacity),
+		receivedMsgC: make(chan *Message, defaultClientReceiveChanCapacity),
+		sendMsgC:     make(chan *Message, defaultClientSendChanCapacity),
+
+		closeC:  make(chan struct{}),
+		notifyC: make(chan struct{}, 2),
+	}
+
+	id, err := client.connect(udpAddr)
+
+	if err != nil {
+		debug("client connect error: %s", err.Error())
+		return nil, err
+	}
+
+	client.connID = id
+	client.connState = stateConnected
+	debug("new client with id: %d", id)
+
+	go client.start()
+
+	return client, nil
+}
+
+func (c *client) connect(addr *lspnet.UDPAddr) (int, error) {
+
+	reqBytes, err := json.Marshal(NewConnect())
+	if err != nil {
+		return 0, err
+	}
+
+	decoder := json.NewDecoder(c.conn)
+
+	var response Message
+
+	reDial := func() error {
+		conn, err := lspnet.DialUDP("udp", nil, addr)
+		if err != nil {
+			return err
+		}
+		c.conn = conn
+		decoder = json.NewDecoder(c.conn)
+		return nil
+	}
+
+	for i := 0; i < c.params.EpochLimit; i++ {
+
+		debug("client connect try [%d]...", i+1)
+
+		timeout := time.Now().Add(time.Millisecond * time.Duration(c.params.EpochMillis))
+		c.conn.SetDeadline(timeout)
+
+		if _, err = c.conn.Write(reqBytes); err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				debug("client write timeout")
+				continue
+			} else {
+				debug("write udp error: %s\n", err.Error())
+				now := time.Now()
+				if now.Before(timeout) {
+					time.Sleep(timeout.Sub(now))
+				}
+				c.conn.Close()
+				reDial()
+				continue
+			}
+
+		}
+
+		if err := decoder.Decode(&response); err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				debug("client read timeout")
+				continue
+			} else {
+				debug("read udp error: %s\n", err.Error())
+				now := time.Now()
+				if now.Before(timeout) {
+					time.Sleep(timeout.Sub(now))
+				}
+				c.conn.Close()
+				reDial()
+				continue
+			}
+
+		}
+
+		debug("client receive: %s\n", response.String())
+
+		if response.Type == MsgAck {
+			c.conn.SetDeadline(time.Time{})
+			debug("client connect succeeded\n")
+			return response.ConnID, nil
+		}
+	}
+
+	return 0, errors.New("connect timeout")
+}
+
+func (c *client) send(msg *Message) {
+	debug("client [%d] send message: %s\n", c.connID, msg.String())
+	data, _ := json.Marshal(msg)
+	c.conn.Write(data)
+}
+
+func (c *client) sendData(msg *Message, count int) {
+	c.send(msg)
+	c.sendBuf[msg.SeqNum] = newInFlyMessage(
+		time.Now().Add(time.Millisecond*time.Duration(c.params.EpochMillis)),
+		count,
+		msg)
+}
+
+func (c *client) resetTimer(duration time.Duration) {
+
+	if c.timer == nil {
+		c.timer = time.NewTimer(duration)
+	} else {
+		if !c.timer.Stop() {
+			select {
+			case <-c.timer.C:
+			default:
+			}
+		}
+		c.timer.Reset(duration)
+	}
+}
+
+func (c *client) start() {
+
+	readFunc := func() {
+		defer func() {
+			debug("client read goroutine exit")
+			c.notifyC <- struct{}{}
+		}()
+
+		decoder := json.NewDecoder(c.conn)
+
+		for {
+			msg := new(Message)
+
+			if err := decoder.Decode(msg); err != nil {
+				debug("client [%d] decode message error: %s\n", c.connID, err.Error())
+				atomic.StoreInt32(&c.connState, stateClosed)
+				close(c.receivedMsgC)
+				return
+			} else {
+				c.receiveMsgC <- msg
+			}
+		}
+	}
+
+	go readFunc()
+
+	defer func() {
+		debug("client message processing goroutine exit")
+		c.conn.Close()
+		c.notifyC <- struct{}{}
+	}()
+
+	onReceiveData := func(msg *Message) {
+
+		debug("client [%d] receive data: %s\n", c.connID, msg.String())
+
+		if atomic.LoadInt32(&c.connState) == stateClosed {
+			debug("client [%d] receive data after closed\n", c.connID)
+			return
+		}
+
+		if msg.SeqNum == c.receivedDataSeq+1 {
+
+			c.receivedDataSeq++
+
+			c.send(NewAck(c.connID, msg.SeqNum))
+
+			if msg.Type == msgClose {
+				// current connection has closed my peer
+				atomic.StoreInt32(&c.connState, stateClosed)
+				return
+			}
+			debug("client delivery message: %s\n", msg.String())
+			c.receivedMsgC <- msg
+
+			if c.receiveBuf.Len() != 0 {
+				toRemove := make([]*list.Element, 0)
+
+				for elem := c.receiveBuf.Front(); elem != c.receiveBuf.Back(); elem = elem.Next() {
+					m := elem.Value.(*Message)
+
+					if m.SeqNum < c.receivedDataSeq+1 {
+						toRemove = append(toRemove, elem)
+					} else if m.SeqNum == c.receivedDataSeq+1 {
+						toRemove = append(toRemove, elem)
+						c.receivedDataSeq++
+						c.send(NewAck(c.connID, m.SeqNum))
+
+						if m.Type == msgClose {
+							// current connection has closed my peer
+							atomic.StoreInt32(&c.connState, stateClosed)
+							return
+						}
+						debug("client delivery message: %s\n", m.String())
+						c.receivedMsgC <- m
+					} else {
+						break
+					}
+				}
+
+				for _, elem := range toRemove {
+					c.receiveBuf.Remove(elem)
+				}
+			}
+
+		} else if msg.SeqNum > c.receivedDataSeq+1 {
+			// receive out-of-order message
+			if c.receiveBuf.Len() == 0 || msg.SeqNum < c.receiveBuf.Front().Value.(*Message).SeqNum {
+				c.receiveBuf.PushFront(msg)
+			} else if msg.SeqNum > c.receiveBuf.Back().Value.(*Message).SeqNum {
+				c.receiveBuf.PushBack(msg)
+			} else {
+				for elem := c.receiveBuf.Front(); elem != c.receiveBuf.Back(); elem = elem.Next() {
+					if elem.Value.(*Message).SeqNum > msg.SeqNum {
+						c.receiveBuf.InsertBefore(msg, elem)
+						break
+					}
+				}
+			}
+		} else {
+			c.send(NewAck(c.connID, c.receivedDataSeq))
+		}
+	}
+
+	onReceiveAck := func(msg *Message) {
+
+		debug("client receive ack: %s\n", msg.String())
+		seq := msg.SeqNum
+
+		if seq >= c.lastAckSeq+1 {
+
+			// message seq between [c.lastAckSeq+1,seq] has all received by peer
+			c.windowSize += seq - c.lastAckSeq
+			c.lastAckSeq = seq
+
+			if c.windowSize > c.params.WindowSize {
+				c.windowSize = c.params.WindowSize
+			}
+
+			toRemove := make([]int, 0)
+			for sendSeq := range c.sendBuf {
+				if sendSeq <= seq {
+					toRemove = append(toRemove, sendSeq)
+				}
+			}
+
+			for _, n := range toRemove {
+				delete(c.sendBuf, n)
+			}
+
+			for c.windowSize > 0 && c.pendingBuf.Len() > 0 {
+				front := c.pendingBuf.Front()
+				debug("client [%d] send message: %s\n", c.connID, front.Value.(*Message).String())
+				c.sendData(front.Value.(*Message), 0)
+				c.pendingBuf.Remove(front)
+				c.windowSize--
+			}
+		}
+
+		state := atomic.LoadInt32(&c.connState)
+		if state == stateClosing && c.pendingBuf.Len() == 0 && len(c.sendBuf) == 0 {
+			debug("client closing, all pending messages have sent out\n")
+			atomic.StoreInt32(&c.connState, stateClosed)
+			c.conn.Close()
+		}
+	}
+
+	onSendData := func(msg *Message) {
+
+		if c.windowSize > 0 && c.pendingBuf.Len() == 0 {
+			debug("client [%d] send message: %s\n", c.connID, msg.String())
+			c.sendData(msg, 0)
+			c.windowSize--
+		} else {
+			debug("client pending message: %s\n", msg.String())
+			c.pendingBuf.PushBack(msg)
+		}
+	}
+
+	onTimeout := func() {
+
+		timeout := time.Millisecond * time.Duration(c.params.EpochMillis)
+		now := time.Now()
+
+		for _, t := range c.sendBuf {
+			if t.expireTime.Before(now) {
+				if t.epochCount > c.params.EpochLimit {
+					// connection is assumed to be lost
+					debug("client [%d] connection is lost\n", c.connID)
+					c.connState = stateClosed
+					return
+				} else {
+					debug("client [%d] %d-th resend message: %s\n", c.connID, t.epochCount+1, t.msg.String())
+					c.sendData(t.msg, t.epochCount+1)
+				}
+			} else {
+				// TODO(optimization: find the first time not before current time)
+				next := t.expireTime.Sub(now)
+				if next < timeout {
+					timeout = next
+				}
+			}
+		}
+
+		c.send(NewAck(c.connID, c.receivedDataSeq))
+		c.resetTimer(timeout)
+	}
+
+	c.resetTimer(time.Millisecond * time.Duration(c.params.EpochMillis))
+
+	for {
+		if atomic.LoadInt32(&c.connState) == stateClosed {
+			break
+		}
+
+		select {
+		case <-c.closeC:
+			atomic.StoreInt32(&c.connState, stateClosing)
+
+			if len(c.sendBuf) == 0 && c.pendingBuf.Len() == 0 {
+				return
+			}
+		case <-c.timer.C:
+			onTimeout()
+			if c.connState == stateClosed {
+				return
+			}
+		case msg := <-c.receiveMsgC:
+			switch msg.Type {
+			case MsgAck:
+				onReceiveAck(msg)
+			case MsgConnect:
+				debug("receive connect after connection has established, ignore message")
+			case MsgData:
+				onReceiveData(msg)
+			case msgClose:
+				// connection is closed by peer
+				atomic.StoreInt32(&c.connState, stateClosing)
+				onReceiveData(msg)
+				debug("connection [%d] is closed by peer", c.connID)
+			}
+		case msg := <-c.sendMsgC:
+			if c.connState == stateClosing {
+				continue
+			}
+			onSendData(msg)
+		}
+	}
 }
 
 func (c *client) ConnID() int {
-	return -1
+	return c.connID
 }
 
 func (c *client) Read() ([]byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return nil, errors.New("not yet implemented")
+
+	state := atomic.LoadInt32(&c.connState)
+	if state == stateClosing || state == stateClosed {
+		return nil, ErrClosed
+	}
+
+	select {
+	case msg, ok := <-c.receivedMsgC:
+		if ok {
+			debug("client Read() return message: %s\n", msg.String())
+			return msg.Payload, nil
+		} else {
+			return nil, ErrClosed
+		}
+	case <-c.closeC:
+		return nil, ErrClosed
+	}
 }
 
 func (c *client) Write(payload []byte) error {
-	return errors.New("not yet implemented")
+	state := atomic.LoadInt32(&c.connState)
+	if state == stateClosing || state == stateClosed {
+		return ErrClosed
+	}
+
+	c.seqNum++
+	msg := NewData(c.connID, c.seqNum, len(payload), payload)
+
+	c.sendMsgC <- msg
+
+	return nil
 }
 
 func (c *client) Close() error {
-	return errors.New("not yet implemented")
+
+	debug("client [%d] closing...\n", c.connID)
+
+	state := atomic.LoadInt32(&c.connState)
+	if state == stateClosing || state == stateClosed {
+		return ErrClosed
+	}
+
+	atomic.StoreInt32(&c.connState, stateClosing)
+
+	c.closeC <- struct{}{}
+
+	<-c.notifyC
+	<-c.notifyC
+
+	if atomic.CompareAndSwapInt32(&c.connState, stateClosing, stateClosed) {
+		close(c.closeC)
+	}
+
+	debug("client [%d] closed\n", c.connID)
+	return nil
 }
--- p1/src/github.com/cmu440/lsp/server_impl.go
+++ p1/src/github.com/cmu440/lsp/server_impl.go
@@ -2,10 +2,90 @@
 
 package lsp
 
-import "errors"
+import (
+	"errors"
+	"strconv"
+	"sync"
+	"encoding/json"
+	"fmt"
+	"container/list"
+	"time"
+	"github.com/cmu440/lspnet"
+)
+
+var (
+	ErrNotExist     = errors.New("connection does not exist")
+	ErrClosed       = errors.New("connection already closed")
+	ErrClientClosed = errors.New("client already closed")
+	ErrClientLost   = errors.New("some client connections has lost")
+)
+
+const (
+	defaultReceiveCapacity = 1024
+	defaultSendCapacity    = 1024
+
+	msgClose MsgType = MsgType(0xff)
+)
+
+type receiveMsg struct {
+	msg        *Message
+	remoteAddr *lspnet.UDPAddr
+}
+
+func newReceiveMsg(msg *Message, addr *lspnet.UDPAddr) *receiveMsg {
+	return &receiveMsg{msg, addr}
+}
+
+type sendMsg struct {
+	msg        *Message
+	remoteAddr *lspnet.UDPAddr
+}
+
+func newSendMsg(msg *Message, addr *lspnet.UDPAddr) *sendMsg {
+	return &sendMsg{msg, addr}
+}
+
+type deliveryMsgType int
+
+const (
+	msgClientClosed deliveryMsgType = iota
+	msgDataReceived
+)
+
+type deliveryMsg struct {
+	type_  deliveryMsgType
+	connId int
+	data   []byte
+}
+
+func newDeliveryMsg(msgType deliveryMsgType, connId int, data []byte) *deliveryMsg {
+	return &deliveryMsg{msgType, connId, data}
+}
 
 type server struct {
-	// TODO: implement this!
+	params *Params
+	nextID int
+	conn   *lspnet.UDPConn
+
+	readC chan *deliveryMsg // deliver message to Read function
+	// spawned client connections notify server it has shutdown.
+	// if server received false, client connections has lost
+	notifyC chan bool
+
+	receivedC chan *Message // received message but not have read by application
+	msgSendC  chan *sendMsg
+
+	inboundC  chan *receiveMsg
+	outboundC chan *sendMsg
+
+	isClosed bool
+	inClose  bool
+	closeC   chan struct{}
+
+	mu          *sync.Mutex
+	cond        *sync.Cond
+	addr2id     map[string]int
+	connections map[int]*clientConn
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -15,23 +95,479 @@ type server struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	return nil, errors.New("not yet implemented")
+
+	udpAddr, err := lspnet.ResolveUDPAddr("udp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return nil, err
+	}
+	conn, err := lspnet.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	server := server{
+		params: params,
+		conn:   conn,
+		nextID: 0,
+
+		readC:     make(chan *deliveryMsg, defaultReceiveCapacity),
+		inboundC:  make(chan *receiveMsg, defaultReceiveCapacity),
+		msgSendC:  make(chan *sendMsg, defaultSendCapacity),
+		outboundC: make(chan *sendMsg, defaultSendCapacity),
+
+		mu:          &sync.Mutex{},
+		closeC:      make(chan struct{}, 1),
+		addr2id:     make(map[string]int),
+		connections: make(map[int]*clientConn),
+	}
+
+	server.cond = sync.NewCond(server.mu)
+
+	debug("server start...\n")
+	go server.start()
+
+	return &server, nil
+}
+
+func (s *server) start() {
+
+	go s.receiveMessage()
+
+	for {
+		select {
+		case <-s.closeC:
+			s.inClose = true
+		case msg := <-s.inboundC:
+			if s.isClosed {
+				break
+			}
+
+			if msg.msg.Type == MsgConnect {
+				if !s.inClose {
+					s.handleConnect(msg.remoteAddr)
+				}
+			} else {
+				// delivery receive message to corresponding clientConn to handle
+				address := msg.remoteAddr.String()
+				var (
+					id   int
+					conn *clientConn
+					ok   bool
+				)
+				s.mu.Lock()
+				id, ok = s.addr2id[address]
+				if ok {
+					conn, ok = s.connections[id]
+				}
+				s.mu.Unlock()
+				if ok {
+					conn.receiveMsg(msg.msg)
+				}
+			}
+
+		case msg := <-s.outboundC:
+			if data, err := json.Marshal(msg.msg); err != nil {
+				debug("encode message error: %s\n", err.Error())
+				panic(err)
+			} else {
+				debug("server send message: %s\n", msg.msg.String())
+				s.conn.WriteToUDP(data, msg.remoteAddr)
+			}
+		}
+	}
+}
+
+func (s *server) handleConnect(addr *lspnet.UDPAddr) {
+	s.mu.Lock()
+	id, ok := s.addr2id[addr.String()]
+	if !ok {
+		id = s.nextConnID()
+		debug("server new client connection [%d]\n", id)
+		address := addr.String()
+		s.addr2id[address] = id
+		client := &clientConn{
+			id:              id,
+			seqNum:          0,
+			address:         address,
+			addr:            addr,
+			s:               s,
+			windowSize:      s.params.WindowSize,
+			params:          s.params,
+			pendingBuf:      list.New(),
+			sendBuf:         make(map[int]*inFlyMessage),
+			lastAckSeq:      0,
+			receiveBuf:      list.New(),
+			receivedDataSeq: 0,
+			receivedMsgC:    s.readC,
+			inboundC:        make(chan *Message, defaultReceiveCapacity),
+			outboundC:       make(chan *Message, defaultSendCapacity),
+
+			inClose:  false,
+			isClosed: false,
+			closeC:   make(chan struct{}, 1),
+		}
+		s.connections[id] = client
+
+		go client.start()
+	}
+	s.mu.Unlock()
+
+	ack := NewAck(id, 0)
+	s.send(ack, addr)
+}
+
+func (s *server) receiveMessage() {
+	data := make([]byte, 65536)
+
+	for {
+		n, addr, err := s.conn.ReadFromUDP(data[0:])
+		if err != nil {
+			break
+		}
+		msg := new(Message)
+
+		if err := json.Unmarshal(data[:n], msg); err != nil {
+			debug("receive corrupted message from %s\n", addr.String())
+			continue
+		}
+
+		s.inboundC <- newReceiveMsg(msg, addr)
+	}
+}
+
+func (s *server) send(msg *Message, addr *lspnet.UDPAddr) {
+	s.outboundC <- newSendMsg(msg, addr)
+}
+
+func (s *server) nextConnID() int {
+	s.nextID++
+	return s.nextID
 }
 
 func (s *server) Read() (int, []byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return -1, nil, errors.New("not yet implemented")
+	select {
+	case <-s.closeC:
+		return 0, nil, ErrClosed
+	case msg := <-s.readC:
+		switch msg.type_ {
+		case msgDataReceived:
+			return msg.connId, msg.data, nil
+		case msgClientClosed:
+			return msg.connId, nil, ErrClientClosed
+		default:
+			panic(fmt.Sprintf("unrecognized message type: %d", msg.type_))
+		}
+	}
 }
 
 func (s *server) Write(connID int, payload []byte) error {
-	return errors.New("not yet implemented")
+	s.mu.Lock()
+	client, ok := s.connections[connID]
+	s.mu.Unlock()
+
+	if !ok {
+		return ErrClosed
+	}
+
+	client.write(payload, false)
+	return nil
 }
 
 func (s *server) CloseConn(connID int) error {
-	return errors.New("not yet implemented")
+	s.mu.Lock()
+	client, ok := s.connections[connID]
+	s.mu.Unlock()
+	if !ok {
+		return ErrNotExist
+	}
+	client.close()
+	return nil
 }
 
 func (s *server) Close() error {
-	return errors.New("not yet implemented")
+
+	debug("server closing...\n")
+
+	s.closeC <- struct{}{}
+
+	s.mu.Lock()
+
+	for _, conn := range s.connections {
+		conn.close()
+	}
+
+	for len(s.connections) != 0 {
+		s.cond.Wait()
+	}
+	s.mu.Unlock()
+
+	s.conn.Close()
+	debug("server closed\n")
+	return nil
+}
+
+func (s *server) removeClient(address string, connId int) {
+	debug("server remove client [%d]\n", connId)
+	s.mu.Lock()
+	delete(s.connections, connId)
+	delete(s.addr2id, address)
+	s.cond.Signal()
+	s.mu.Unlock()
+}
+
+type clientConn struct {
+	id     int
+	seqNum int
+
+	address string
+	addr    *lspnet.UDPAddr
+	s       *server
+
+	windowSize int
+	params     *Params
+
+	pendingBuf *list.List
+	sendBuf    map[int]*inFlyMessage
+	lastAckSeq int
+
+	receiveBuf      *list.List
+	receivedDataSeq int
+	receivedMsgC    chan<- *deliveryMsg
+
+	inboundC  chan *Message
+	outboundC chan *Message
+
+	timer *time.Timer
+	mu    sync.Mutex
+
+	inClose  bool
+	isClosed bool
+	closeC   chan struct{}
+}
+
+func (c *clientConn) close() {
+	debug("server close connection between client %d...\n", c.id)
+	c.closeC <- struct{}{}
+}
+
+func (c *clientConn) send(msg *Message) {
+	c.s.send(msg, c.addr)
+}
+
+func (c *clientConn) sendData(msg *Message, count int) {
+	c.send(msg)
+	c.sendBuf[msg.SeqNum] = newInFlyMessage(
+		time.Now().Add(time.Millisecond*time.Duration(c.params.EpochMillis)),
+		count,
+		msg)
+}
+
+func (c *clientConn) nextSeqNum() int {
+	c.seqNum++
+	return c.seqNum
+}
+
+func (c *clientConn) write(payload []byte, close bool) {
+
+	var msg *Message
+	if !close {
+		msg = NewData(c.id, c.nextSeqNum(), len(payload), payload)
+	} else {
+		msg = &Message{Type: msgClose, ConnID: c.id, SeqNum: c.nextSeqNum()}
+	}
+
+	select {
+	case c.outboundC <- msg:
+	default:
+		c.mu.Lock()
+		defer c.mu.Unlock()
+		c.pendingBuf.PushBack(msg)
+	}
+}
+
+func (c *clientConn) onSendData(msg *Message) {
+	c.mu.Lock()
+	if c.windowSize > 0 && c.pendingBuf.Len() == 0 {
+		c.mu.Unlock()
+		c.sendData(msg, 0)
+		c.windowSize--
+	} else {
+		debug("server client [%d] pending message: %s\n", c.id, msg.String())
+		c.pendingBuf.PushBack(msg)
+		c.mu.Unlock()
+	}
+}
+
+func (c *clientConn) onReceiveData(msg *Message) {
+
+	if msg.SeqNum == c.receivedDataSeq+1 {
+		c.receivedDataSeq++
+		c.send(NewAck(c.id, msg.SeqNum))
+		c.receivedMsgC <- newDeliveryMsg(msgDataReceived, c.id, msg.Payload)
+
+		if c.receiveBuf.Len() != 0 {
+			toRemove := make([]*list.Element, 0)
+
+			for elem := c.receiveBuf.Front(); elem != c.receiveBuf.Back(); elem = elem.Next() {
+				m := elem.Value.(*Message)
+				if m.SeqNum < c.receivedDataSeq+1 {
+					toRemove = append(toRemove, elem)
+				} else if m.SeqNum == c.receivedDataSeq+1 {
+					toRemove = append(toRemove, elem)
+					c.receivedDataSeq++
+					c.send(NewAck(c.id, m.SeqNum))
+					c.receivedMsgC <- newDeliveryMsg(msgDataReceived, c.id, m.Payload)
+				} else {
+					break
+				}
+			}
+		}
+	} else if msg.SeqNum > c.receivedDataSeq+1 {
+		// receive out-of-order message
+		if c.receiveBuf.Len() == 0 || msg.SeqNum < c.receiveBuf.Front().Value.(*Message).SeqNum {
+			c.receiveBuf.PushFront(msg)
+		} else if msg.SeqNum > c.receiveBuf.Back().Value.(*Message).SeqNum {
+			c.receiveBuf.PushBack(msg)
+		} else {
+			for elem := c.receiveBuf.Front(); elem != c.receiveBuf.Back(); elem = elem.Next() {
+				if elem.Value.(*Message).SeqNum > msg.SeqNum {
+					c.receiveBuf.InsertBefore(msg, elem)
+					break
+				}
+			}
+		}
+	} else {
+		c.send(NewAck(c.id, c.receivedDataSeq))
+	}
+}
+
+func (c *clientConn) onReceiveAck(msg *Message) {
+
+	debug("server client [%d] receive ack: %s\n", c.id, msg.String())
+
+	seq := msg.SeqNum
+
+	if seq >= c.lastAckSeq+1 {
+		c.windowSize += seq - c.lastAckSeq
+		c.lastAckSeq = seq
+
+		if c.windowSize > c.params.WindowSize {
+			c.windowSize = c.params.WindowSize
+		}
+
+		toRemove := make([]int, 0)
+		for sendSeq := range c.sendBuf {
+			if sendSeq <= seq {
+				toRemove = append(toRemove, sendSeq)
+			}
+		}
+		for _, n := range toRemove {
+			delete(c.sendBuf, n)
+		}
+
+		for c.windowSize > 0 && c.pendingBuf.Len() > 0 {
+			front := c.pendingBuf.Front()
+			c.sendData(front.Value.(*Message), 0)
+			c.pendingBuf.Remove(front)
+			c.windowSize--
+		}
+	}
+
+	if c.inClose {
+		if len(c.sendBuf) == 0 && c.pendingBuf.Len() == 0 {
+			debug("server connection between client [%d] has closed\n", c.id)
+			c.isClosed = true
+		} else {
+			debug("sendBuf: %d, pendingBuf: %d\n", len(c.sendBuf), c.pendingBuf.Len())
+		}
+	}
+}
+
+func (c *clientConn) onTimeout() {
+	timeout := time.Millisecond * time.Duration(c.params.EpochMillis)
+	now := time.Now()
+
+	for _, t := range c.sendBuf {
+		if t.expireTime.Before(now) {
+			if t.epochCount > c.params.EpochLimit {
+				debug("server client [%d] connection is lost", c.id)
+				c.receivedMsgC <- newDeliveryMsg(msgClientClosed, c.id, nil)
+				c.isClosed = true
+				return
+			} else {
+				debug("server client [%d] %d-th resend message: %s\n", c.id, t.epochCount+1, t.msg.String())
+				c.sendData(t.msg, t.epochCount+1)
+			}
+		} else {
+			next := t.expireTime.Sub(now)
+			if next < timeout {
+				timeout = next
+			}
+		}
+	}
+
+	c.send(NewAck(c.id, c.receivedDataSeq))
+	c.resetTimer(timeout)
+}
+
+func (c *clientConn) receiveMsg(msg *Message) {
+	c.inboundC <- msg
+}
+
+func (c *clientConn) resetTimer(duration time.Duration) {
+	if c.timer == nil {
+		c.timer = time.NewTimer(duration)
+	} else {
+		if !c.timer.Stop() {
+			select {
+			case <-c.timer.C:
+			default:
+			}
+		}
+		c.timer.Reset(duration)
+	}
+}
+
+func (c *clientConn) start() {
+
+	defer func() {
+		c.s.removeClient(c.address, c.id)
+	}()
+
+	c.resetTimer(time.Millisecond * time.Duration(c.params.EpochMillis))
+
+	for {
+		if c.isClosed {
+			break
+		}
+		select {
+		case <-c.closeC:
+			debug("server client [%d] in closing...\n", c.id)
+			c.inClose = true
+			c.write(nil, true) // notify peer to close connection
+		case <-c.timer.C:
+			c.onTimeout()
+			if c.isClosed {
+				return
+			}
+		case msg := <-c.outboundC:
+			if (c.inClose || c.isClosed) && msg.Type != msgClose {
+				// stop send new data if current connection is decided to close
+				continue
+			}
+			c.onSendData(msg)
+		case msg := <-c.inboundC:
+			debug("server [%d] receive message: %s\n", c.id, msg.String())
+			switch msg.Type {
+			case MsgConnect:
+				debug("server [%d] receive connect after connection has established, ignore message", c.id)
+			case MsgData:
+				c.onReceiveData(msg)
+			case MsgAck:
+				c.onReceiveAck(msg)
+			case msgClose:
+				// connection closed by peer
+			}
+		}
+	}
 }
